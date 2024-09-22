9d94e51e3ea009406d1d9dd21a7cddf2d603e83c
Andrew Palchykovsky <dangerok2@gmail.com>
2024-09-22 20:05:12 +0300

    feat: create card

---
+++ src/components/CreateCard.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const title = ref('')
+const description = ref('')
+
+const addItem = () => {
+  console.log(title.value, description.value)
+}
+</script>
+
+<template>
+  <v-card class="mx-auto my-5" width="400">
+    <v-form class="pa-2" @submit.prevent>
+      <v-text-field v-model.trim="title" label="Title"></v-text-field>
+      <v-text-field v-model.trim="description" label="Description"></v-text-field>
+      <v-btn block class="my-2" color="indigo-darken-3" @click="addItem()">Submit</v-btn>
+    </v-form>
+  </v-card>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import TheCard from '@/components/TheCard.vue'
+import CreateCard from '@/components/CreateCard.vue'
 </script>
 
 <template>
   <main>
-    <TheCard></TheCard>
+    <TheCard />
+    <CreateCard />
   </main>
 </template>
